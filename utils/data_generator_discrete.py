5d290619fc6be0e56a0cc4a476871a801422dfc2
Natsuki Yamashita <yamashita.natsuki.r6@dc.tohoku.ac.jp>
2025-12-02 14:54:53 +0900

    commit

--- utils/data_generator_discrete.py
+++ utils/data_generator_discrete.py
@@ -6,10 +6,10 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 from multiprocessing import get_context
 import argparse
 
-from .likelihood import Likelihood_diagonal_exp
-from .formate_matrix_toMLData import formate_dataMatrix, matrix_trimer
+# from .likelihood import Likelihood_diagonal_exp
+from formate_matrix_toMLData import formate_dataMatrix, matrix_trimer
 
-from .data_generator import (
+from data_generator import (
     DataGenerator,
     DiagonalTransitionRateMatrixGenerator,
 )
@@ -44,13 +44,13 @@ def _seed_for_index(base_seed: int, idx: int) -> int:
     ss = np.random.SeedSequence([base_seed, idx])
     return int(ss.generate_state(1)[0])
 
-def _insert_likelihood_results(M: np.ndarray,) -> np.ndarray:
-    mt = matrix_trimer(M)
-    data = mt.trim_data(start=3)
-    ll = Likelihood_diagonal_exp(data, num_state=4)
-    Q_ll = ll.optimize(np.array([-0.5,-1,-1.5]))
-    new_M = np.insert(M,4,Q_ll,axis=0)
-    return new_M
+# def _insert_likelihood_results(M: np.ndarray,) -> np.ndarray:
+#     mt = matrix_trimer(M)
+#     data = mt.trim_data(start=3)
+#     ll = Likelihood_diagonal_exp(data, num_state=4)
+#     Q_ll = ll.optimize(np.array([-0.5,-1,-1.5]))
+#     new_M = np.insert(M,4,Q_ll,axis=0)
+#     return new_M
 
 
 def _one_dataset_job(idx: int, out_dir: str, states: int, lifespan: float,
@@ -75,7 +75,7 @@ def _one_dataset_job(idx: int, out_dir: str, states: int, lifespan: float,
     # n_intervals をランダム化した DirichletDeltaT を利用
     del_t_gen = DirichletDeltaT(min_intervals=2, max_intervals=10, rng=rng)
     M = dg.generate_matrix(del_t_gen.sample)
-    M = _insert_likelihood_results(M)
+    # M = _insert_likelihood_results(M)
     # CSV出力
     dg.generate_dataFile(M, name, str(out))
     return str(out / name)
@@ -85,7 +85,7 @@ def _one_dataset_job(idx: int, out_dir: str, states: int, lifespan: float,
 def _parse_args_parallel():
     p = argparse.ArgumentParser(description="Parallel Dirichlet delta_t generator")
     p.add_argument("--count", type=int, required=True, help="生成するデータセット数")
-    p.add_argument("--out-dir", type=str, required=True, help="出力ディレクトリ")
+    p.add_argument("--out-dir", type=str, help="出力ディレクトリ")
     p.add_argument("--states", type=int, default=4, help="状態数")
     p.add_argument("--lifespan", type=float, default=100.0, help="寿命パラメータ")
     p.add_argument("--min-n", type=int, default=5000, help="最小サンプル数")
@@ -119,21 +119,21 @@ def _run_parallel_from_args(args):
 if __name__ == "__main__":
     import copy
     base_args = _parse_args_parallel()
-    l = [
-        100,200,400,600,800,1000
-    ]
+    # l = [
+    #     100,200,400,600,800,1000
+    # ]
     if not base_args.run_parallel:
         raise SystemExit("run_parallel を有効にしてください")
     
-    for n in l:
-        args = copy.deepcopy(base_args)
-        print(args.base_seed)
-        args.base_seed += n
-        print(args.base_seed)
-        args.min_n = n
-        args.max_n = n
-        args.out_dir = base_args.out_dir + f"/testdata_n{n}"
-        _run_parallel_from_args(args)
-
+    # for n in l:
+    #     args = copy.deepcopy(base_args)
+    #     print(args.base_seed)
+    #     args.base_seed += n
+    #     print(args.base_seed)
+    #     args.min_n = n
+    #     args.max_n = n
+    #     args.out_dir = base_args.out_dir + f"/testdata_n{n}"
+    #     _run_parallel_from_args(args)
+    _run_parallel_from_args(args=base_args)
     
         
